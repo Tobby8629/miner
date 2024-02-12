de6d31b4d3fa4d3376cf2ad40215c0da0786cb47
tobby8629 <sarhmullah15@gmail.com>
2024-02-12 14:35:33 +0100

    Create onboarding page

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes, faUserAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -21,10 +21,20 @@ const Navbar = () => {
          <h1>LOGO</h1>
         </Link>
        </div>
-       <div className={toogle ? styles.nav_show : styles.nav_menu}>
-        {Links.map((link)=>(
-         <Link to={link?.link} key={link?.name}>{link?.name}</Link>
-        ))}
+       <div className={`${styles.nav_menu} ${toogle ? styles.nav_show : ''}`}>
+        <div className={styles.links}>
+          {Links.map((link)=>(
+          <Link to={link?.link} key={link?.name}>{link?.name}</Link>
+          ))}
+        </div>
+        <Link className={styles.profile}>
+          <span className={styles.initials}>
+            <FontAwesomeIcon icon={faUserAlt} />
+          </span>
+          <span className={styles.name}>
+            <p>Lucas Boss</p>
+          </span>
+        </Link>
        </div>
     </nav>
   )
