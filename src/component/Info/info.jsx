0ec24c603597d9547f86e0b6b649085e8fe42584
Ayomipo Majek <ayomipomajek@gmail.com>
2024-03-13 05:42:12 -1200

    contact form added to the about page

--- src/component/Info/info.jsx
+++ src/component/Info/info.jsx
@@ -22,39 +22,49 @@ const info = () => {
 
       <div className="whyUs">
         <h1>Why Choose Us ? <span className='dot'></span></h1>
-        <ul>
-          <li>
+        <div className="whyPara">
+          <p>
             <b>User-Focused Experience:</b> We prioritize user experience and strive to make our
             platform intuitive and easy to use for investors of all levels. So feel free to explore
             every feature.
-          </li>
-          <li>
+          </p>
+          <p>
             <b>Security First:</b> The security of your assets is our top priority. We employ
             industry-leading security measures to safeguard your investments and personal information.
-          </li>
-          <li>
+          </p>
+          <p>
             <b>Transparency:</b> We believe in transparency and provide our users with access to real-time data,
             up-to-date market insights, and comprehensive investment analytics.
-          </li>
-          <li>
+          </p>
+          <p>
             <b>Diverse Investment Options:</b> From Bitcoin and Ethereum to emerging altcoins, we offer a
             diverse range of investment options to help you build a well-rounded crypto portfolio.
-          </li>
-          <li>
+          </p>
+          <p>
             <b>Customer Support:</b> Our dedicated customer support team is 
             available to assist you every step of the way. Whether you have 
-            questions about investing or need technical assistance, we're here to help.
-          </li>
-        </ul>
+            questions or need technical assistance, we're here to help.
+          </p>
+        </div>
       </div>
 
       <div className="getStarted">
-        <h1>Get Started Today <span className='dot'></span></h1>
+        <h1>Get Started Today <span className="dot"></span></h1>
         <p>
           Join thousands of investors who have chosen Crypto Gland as their trusted partner in the
           world of cryptocurrencies. Sign up today to start investing,
           and let's navigate the exciting world of digital assets together.
         </p>
+      </div>
+
+      <div className="contactUs">
+        <h1>Contact Us <span className='dot'></span></h1>
+        <form className="contactForm" action="https://formspree.io/" method="POST">
+          <input className="name" type="text" placeholder="Name" maxLength="30" required/>
+          <input className="mail" type="email" placeholder="Email" required/>
+          <textarea className="message" placeholder="Message" required></textarea>
+          <button className="submit" type="submit">Submit</button>
+        </form>
       </div>
     </section>
   )
